824429c7ce4df0bd7aa2e7f2f0e3dde21bb4a847
Halit <hltcamm@gmail.com>
2022-12-14 16:26:54 +0300

    for loops  added

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+# 1
+agg=0
+for x in range(101):
+    agg+=x
+print(agg) 
+
+# 2
+for x in "abc": #for string variable, x take value which is each character of "abc" string
+    print(x)
+
+# 3
+    for x in range(5):
+    print(x)
+
+# 4
+ x='hey''
+index=0
+while index<=len(x):
+    print(x[index])
+    index+=1
+# 5    
